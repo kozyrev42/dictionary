63114e3d44e63091bf0ffbb4abf35e8f8080cd42
kozyrev42 <kozyrev054@gmail.com>
2022-10-11 21:49:38 +0700

    мелкие правки

--- delete_word.php
+++ delete_word.php
@@ -55,6 +55,248 @@ $words = get_all_words();
         <?php endif;?>
     <?php endforeach;?>
 
+    <h1>Г</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "г"):?> 
+            <a href="delete_handler.php?id=<?php echo $word['id'] ?>"> <?php echo $text ?> </a> <br/>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Д</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "д"):?> 
+            <a href="delete_handler.php?id=<?php echo $word['id'] ?>"> <?php echo $text ?> </a> <br/>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Е</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "е"):?> 
+            <a href="delete_handler.php?id=<?php echo $word['id'] ?>"> <?php echo $text ?> </a> <br/>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Ё</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "ё"):?> 
+            <a href="delete_handler.php?id=<?php echo $word['id'] ?>"> <?php echo $text ?> </a> <br/>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Ж</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "ж"):?> 
+            <a href="delete_handler.php?id=<?php echo $word['id'] ?>"> <?php echo $text ?> </a> <br/>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>З</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "з"):?> 
+            <a href="delete_handler.php?id=<?php echo $word['id'] ?>"> <?php echo $text ?> </a> <br/>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>И</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "и"):?> 
+            <a href="delete_handler.php?id=<?php echo $word['id'] ?>"> <?php echo $text ?> </a> <br/>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Й</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "й"):?> 
+            <a href="delete_handler.php?id=<?php echo $word['id'] ?>"> <?php echo $text ?> </a> <br/>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>К</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "к"):?> 
+            <a href="delete_handler.php?id=<?php echo $word['id'] ?>"> <?php echo $text ?> </a> <br/>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Л</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "л"):?> 
+            <a href="delete_handler.php?id=<?php echo $word['id'] ?>"> <?php echo $text ?> </a> <br/>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>М</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "м"):?> 
+            <a href="delete_handler.php?id=<?php echo $word['id'] ?>"> <?php echo $text ?> </a> <br/>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Н</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "н"):?> 
+            <a href="delete_handler.php?id=<?php echo $word['id'] ?>"> <?php echo $text ?> </a> <br/>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>О</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "о"):?> 
+            <a href="delete_handler.php?id=<?php echo $word['id'] ?>"> <?php echo $text ?> </a> <br/>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>П</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "п"):?> 
+            <a href="delete_handler.php?id=<?php echo $word['id'] ?>"> <?php echo $text ?> </a> <br/>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Р</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "р"):?> 
+            <a href="delete_handler.php?id=<?php echo $word['id'] ?>"> <?php echo $text ?> </a> <br/>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>С</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "с"):?> 
+            <a href="delete_handler.php?id=<?php echo $word['id'] ?>"> <?php echo $text ?> </a> <br/>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Т</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "т"):?> 
+            <a href="delete_handler.php?id=<?php echo $word['id'] ?>"> <?php echo $text ?> </a> <br/>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>У</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "у"):?> 
+            <a href="delete_handler.php?id=<?php echo $word['id'] ?>"> <?php echo $text ?> </a> <br/>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Ф</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "ф"):?> 
+            <a href="delete_handler.php?id=<?php echo $word['id'] ?>"> <?php echo $text ?> </a> <br/>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Х</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "х"):?> 
+            <a href="delete_handler.php?id=<?php echo $word['id'] ?>"> <?php echo $text ?> </a> <br/>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Ц</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "ц"):?> 
+            <a href="delete_handler.php?id=<?php echo $word['id'] ?>"> <?php echo $text ?> </a> <br/>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Ч</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "ч"):?> 
+            <a href="delete_handler.php?id=<?php echo $word['id'] ?>"> <?php echo $text ?> </a> <br/>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Ш</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "ш"):?> 
+            <a href="delete_handler.php?id=<?php echo $word['id'] ?>"> <?php echo $text ?> </a> <br/>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Щ</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "щ"):?> 
+            <a href="delete_handler.php?id=<?php echo $word['id'] ?>"> <?php echo $text ?> </a> <br/>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Э</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "э"):?> 
+            <a href="delete_handler.php?id=<?php echo $word['id'] ?>"> <?php echo $text ?> </a> <br/>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Ю</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "ю"):?> 
+            <a href="delete_handler.php?id=<?php echo $word['id'] ?>"> <?php echo $text ?> </a> <br/>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Я</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "я"):?> 
+            <a href="delete_handler.php?id=<?php echo $word['id'] ?>"> <?php echo $text ?> </a> <br/>
+        <?php endif;?>
+    <?php endforeach;?>
 
 </body>
 </html>
--- index.php
+++ index.php
@@ -52,7 +52,248 @@ $words = get_all_words();
         <?php endif;?>
     <?php endforeach;?>
 
+    <h1>Г</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "г"):?> 
+            <h4> <ul type="square"><li><?php echo $text ?></li></ul> </h4>
+        <?php endif;?>
+    <?php endforeach;?>
 
+    <h1>Д</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "д"):?> 
+            <h4> <ul type="square"><li><?php echo $text ?></li></ul> </h4>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Е</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "е"):?> 
+            <h4> <ul type="square"><li><?php echo $text ?></li></ul> </h4>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Ё</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "ё"):?> 
+            <h4> <ul type="square"><li><?php echo $text ?></li></ul> </h4>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Ж</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "ж"):?> 
+            <h4> <ul type="square"><li><?php echo $text ?></li></ul> </h4>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>З</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "з"):?> 
+            <h4> <ul type="square"><li><?php echo $text ?></li></ul> </h4>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>И</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "и"):?> 
+            <h4> <ul type="square"><li><?php echo $text ?></li></ul> </h4>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Й</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "й"):?> 
+            <h4> <ul type="square"><li><?php echo $text ?></li></ul> </h4>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>К</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "к"):?> 
+            <h4> <ul type="square"><li><?php echo $text ?></li></ul> </h4>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Л</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "л"):?> 
+            <h4> <ul type="square"><li><?php echo $text ?></li></ul> </h4>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>М</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "м"):?> 
+            <h4> <ul type="square"><li><?php echo $text ?></li></ul> </h4>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Н</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "н"):?> 
+            <h4> <ul type="square"><li><?php echo $text ?></li></ul> </h4>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>О</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "о"):?> 
+            <h4> <ul type="square"><li><?php echo $text ?></li></ul> </h4>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>П</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "п"):?> 
+            <h4> <ul type="square"><li><?php echo $text ?></li></ul> </h4>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Р</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "р"):?> 
+            <h4> <ul type="square"><li><?php echo $text ?></li></ul> </h4>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>С</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "с"):?> 
+            <h4> <ul type="square"><li><?php echo $text ?></li></ul> </h4>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Т</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "т"):?> 
+            <h4> <ul type="square"><li><?php echo $text ?></li></ul> </h4>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>У</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "у"):?> 
+            <h4> <ul type="square"><li><?php echo $text ?></li></ul> </h4>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Ф</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "ф"):?> 
+            <h4> <ul type="square"><li><?php echo $text ?></li></ul> </h4>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Х</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "х"):?> 
+            <h4> <ul type="square"><li><?php echo $text ?></li></ul> </h4>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Ц</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "ц"):?> 
+            <h4> <ul type="square"><li><?php echo $text ?></li></ul> </h4>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Ч</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "ч"):?> 
+            <h4> <ul type="square"><li><?php echo $text ?></li></ul> </h4>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Ш</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "ш"):?> 
+            <h4> <ul type="square"><li><?php echo $text ?></li></ul> </h4>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Щ</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "щ"):?> 
+            <h4> <ul type="square"><li><?php echo $text ?></li></ul> </h4>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Э</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "э"):?> 
+            <h4> <ul type="square"><li><?php echo $text ?></li></ul> </h4>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Ю</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "ю"):?> 
+            <h4> <ul type="square"><li><?php echo $text ?></li></ul> </h4>
+        <?php endif;?>
+    <?php endforeach;?>
+
+    <h1>Я</h1>
+    <?php foreach ($words as $word) :?>
+        <?php $text = $word['name'];
+              $first_letter = mb_strcut($text, 0, 2, 'UTF-8');?>   
+        <?php if ($first_letter == "я"):?> 
+            <h4> <ul type="square"><li><?php echo $text ?></li></ul> </h4>
+        <?php endif;?>
+    <?php endforeach;?>
 
 </body>
 </html>
